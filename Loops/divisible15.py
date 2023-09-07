6045769d5c25bf7157d7cc8797b85d0c16daa0e3
hdubanp <142341238+hdubanp@users.noreply.github.com>
2023-09-07 12:44:33 -0500

    Update divisible15.py

--- Loops/divisible15.py
+++ Loops/divisible15.py
@@ -1,8 +1,10 @@
-print("inserte numero menor del rango")
+a = input("Digite el numero menor del rango: ")
+a = int(a)
+b = input("Digite el numero mayor del rango: ")
+b = int(b)
 
-print("inserte numero mayor del rango")
 
-for i in range(100, 301): #Lo que hace este codigo es dentro de un rango pre establecido tomar unicamente los numeros que son divisibles de 15 y dan residuo 5#
+for i in range(a, b): #Lo que hace este codigo es dentro de un rango pre establecido tomar unicamente los numeros que son divisibles de 15 y dan residuo 5#
     if (i%15) != 0:
         continue
     print(i)
