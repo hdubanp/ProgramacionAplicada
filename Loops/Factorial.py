f05f4f87eddf024085ad91ad169de7ffb4bc41c5
hdubanp <142341238+hdubanp@users.noreply.github.com>
2023-09-07 12:33:47 -0500

    Update Factorial.py

--- Loops/Factorial.py
+++ Loops/Factorial.py
@@ -1,12 +1,12 @@
 while True:
 
-    value = int(input("Enter a positive integer value: "))
+    value = int(input("Ingrese un numero positivo: "))
     print("Value: ", value)
     a = isinstance(value, int)
     if a == True and value > 0:
         fact = 1
         for i in range (1, value + 1):
             fact = fact*i            
-        print(f'The factorial of {value} is: ', fact)
+        print(f'El factorial de {value} es: ', fact)
     else:
-        print("Please, enter a positive integer number")
+        print("Por favor, digite un numero positivo")
