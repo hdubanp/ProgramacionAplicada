97b1955b0f24baba6174f4e4f3fe75720aad22e4
hdubanp <142341238+hdubanp@users.noreply.github.com>
2023-09-07 13:37:07 -0500

    Update tryt.py

--- Loops/tryt.py
+++ Loops/tryt.py
@@ -1,2 +1,9 @@
-my_cool_variable = 7 + 8 == 11
+a = input("Digite un numero : ")
+a = int(a)
+b = input("Digite otro a sumar numero : ")
+b = int(b)
+c = input("Digite el resultado que cree correcto : ")
+c = int(c)
+
+my_cool_variable = a + b == c
 print(my_cool_variable)
