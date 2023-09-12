9c4f6fcc8ec6db0663eded1862267860b9abff11
hdubanp <142341238+hdubanp@users.noreply.github.com>
2023-09-11 21:55:35 -0500

    Update read csv.py

--- Corte2/read csv.py
+++ Corte2/read csv.py
@@ -1,1 +1,11 @@
-
+import csv  
+def main():  
+    # Para abrir el archivo CSV 
+    with open(' python.csv ', newline = '') as csv_file:  
+        csv_read = csv.reader( csv_file, delimiter = ',')  
+  
+        # Para leer y mostrar cada fila 
+        for row in csv_read:  
+            print(row)  
+if __name__ == '__main__':  
+    main()  
