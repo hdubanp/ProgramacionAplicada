3ba133cad2581b15ebe6da6ed01f01142bf90252
hdubanp <142341238+hdubanp@users.noreply.github.com>
2023-09-11 22:11:51 -0500

    Update fifthProgram.py

--- PrimerosProgramas/fifthProgram.py
+++ PrimerosProgramas/fifthProgram.py
@@ -1,19 +1,27 @@
-coffee_price = 1.50
-number_of_coffees = 4
- 
-# Prints "6.0"
+coffee_price = 1.50  # Define el precio del café como 1.50
+number_of_coffees = 4  # Define la cantidad de cafés como 4
+
+# Imprime el resultado de multiplicar el precio del café por la cantidad de cafés
+# Esto calcula el costo total de los cafés
+# Imprime "6.0" ya que 1.50 * 4 es igual a 6.0
 print(coffee_price * number_of_coffees)
-# Prints "1.5"
+
+# Imprime el precio del café, que es "1.5"
 print(coffee_price)
-# Prints "4"
+
+# Imprime la cantidad de cafés, que es "4"
 print(number_of_coffees)
- 
-# Updating the price 
+
+# Actualiza el precio del café a 2.00
 coffee_price = 2.00
- 
-# Prints "8.0"
+
+# Imprime el resultado de multiplicar el nuevo precio del café por la cantidad de cafés
+# Esto calcula el nuevo costo total de los cafés
+# Imprime "8.0" ya que 2.00 * 4 es igual a 8.0
 print(coffee_price * number_of_coffees)
-# Prints "2.0"
+
+# Imprime el nuevo precio del café, que es "2.0"
 print(coffee_price)
-# Prints "4"
+
+# Imprime la cantidad de cafés, que sigue siendo "4"
 print(number_of_coffees)
