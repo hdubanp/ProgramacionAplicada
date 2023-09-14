34b8d475631359dd78e64cdd4f59d7a4f4a5f1f9
hdubanp <142341238+hdubanp@users.noreply.github.com>
2023-09-14 13:16:27 -0500

    Update whileLoop1.py

--- Loops/whileLoop1.py
+++ Loops/whileLoop1.py
@@ -1,5 +1,12 @@
-for i in range(1,6):
+for i in range(1, 6):
+    # Este bucle while está anidado dentro del bucle for.
     while i <= 4:
-        i += 1
-        print(i)
-    break    
+        i += 1  # Incrementa el valor de 'i' en 1 en cada iteración del bucle while.
+        print(i)  # Imprime el valor actual de 'i'.
+
+    # Una vez que 'i' es mayor que 4, se sale del bucle while.
+    # Luego, se ejecuta la siguiente iteración del bucle for (si corresponde).
+
+    break  # Después de la primera iteración del bucle for, se utiliza 'break' para salir del bucle for.
+
+# Como resultado, este código imprimirá solo una serie de números 2, 3, 4, 5 y luego se detendrá.
