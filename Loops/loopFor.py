0e4e0bad1b97875fe61ffb3bd3609fae5d884c6d
hdubanp <142341238+hdubanp@users.noreply.github.com>
2023-09-14 13:07:34 -0500

    Update loopFor.py

--- Loops/loopFor.py
+++ Loops/loopFor.py
@@ -1,9 +1,16 @@
-import time
+import time  # Importa el módulo 'time' para utilizar la función 'sleep'.
 
-cadena = 'Python'
+cadena = 'Python'  # Define una cadena de caracteres 'Python'.
 
+# Itera a través de cada letra en la cadena 'cadena'.
 for letra in cadena:
-   if letra == 't':
-      continue
-   print(letra)
-   time.sleep(1)
+    if letra == 't':  # Comprueba si la letra actual es 't'.
+        continue  # Si es 't', se salta la iteración actual y pasa a la siguiente letra.
+
+    print(letra)  # Muestra la letra actual en la consola.
+
+    # Pausa la ejecución del programa durante 1 segundo.
+    time.sleep(1)
+
+# El bucle for se completa después de iterar a través de todas las letras de 'cadena'.
+
