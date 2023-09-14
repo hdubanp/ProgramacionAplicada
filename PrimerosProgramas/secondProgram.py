0f1097f1b261c66981c82505a50570040c1790da
hdubanp <142341238+hdubanp@users.noreply.github.com>
2023-09-14 13:26:38 -0500

    Update secondProgram.py

--- PrimerosProgramas/secondProgram.py
+++ PrimerosProgramas/secondProgram.py
@@ -1,8 +1,11 @@
 # Define the release and runtime integer variables below:
+#Son enteros
 release_year = 1977
 runtime = 1980
 
+
 # Define the rating_out_of_10 float variable below: 
+#Es flotante
 rating_out_of_10 = 6.8
 print(type(runtime))
 print(type(rating_out_of_10))
