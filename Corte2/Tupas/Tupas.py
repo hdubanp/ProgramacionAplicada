d659801ce3320d64c6e6390019e09ca4e1abc46b
hdubanp <142341238+hdubanp@users.noreply.github.com>
2023-09-19 13:54:45 -0500

    Update Tupas.py

--- Corte2/Tupas/Tupas.py
+++ Corte2/Tupas/Tupas.py
@@ -1,1 +1,10 @@
 
+def generar_tuplas_de_lista(lista):
+    
+    indice = 0
+    
+    while indice < len(lista)-1:
+        print(lista[indice],lista[indice+1])
+        indice = indice + 1
+        
+print(generar_tuplas_de_lista([64,68,97]))
