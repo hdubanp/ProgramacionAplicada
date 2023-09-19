f4b4e8629082d412baf732839b10c740c0e9131a
hdubanp <142341238+hdubanp@users.noreply.github.com>
2023-09-19 13:55:42 -0500

    Update Excel.py

--- Corte2/Manejo de Archivos/Excel.py
+++ Corte2/Manejo de Archivos/Excel.py
@@ -1,1 +1,17 @@
+#Escribir
+"""f= open("fileejemplos.txt","a")
+f.write('\n'+" añadir nuevo texto")
+f.close()"""
 
+#lectura
+"""f= open("fileejemplos.txt","r")
+print(f.read())
+
+#lectura por linea
+print(f.readline(25))
+
+#Crear nuevo Archivo
+f=open("Nuevoarchivo.txt", "x")"""
+
+#Borrar archivo
+os.remove("Nuevoarchivo.txt")
