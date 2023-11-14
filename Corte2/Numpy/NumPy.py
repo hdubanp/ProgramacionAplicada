de15daccabd1dcf763f5f45a789e4c114f5b10ee
hdubanp <142341238+hdubanp@users.noreply.github.com>
2023-11-14 10:51:38 -0500

    Update NumPy.py

--- Corte2/Numpy/NumPy.py
+++ Corte2/Numpy/NumPy.py
@@ -292,17 +292,207 @@ print(arr)
 # Imprimir el tipo de datos de la matriz utilizando el atributo 'dtype'
 print(arr.dtype)
 
-Para i, u, fy Stambién Upodemos definir el tamaño.
+Para i, u, f y S también Upodemos definir el tamaño.
+
+-Ejemplo 4
+  
+Cree una matriz con tipo de datos entero de 4 bytes:
+# Importar la biblioteca NumPy con el alias 'np'
+import numpy as np
+
+# Crear una matriz NumPy a partir de una lista de enteros con tipo de datos entero de 4 bytes ('i4')
+arr = np.array([1, 2, 3, 4], dtype='i4')
+
+# Imprimir la matriz
+print(arr)
+
+# Imprimir el tipo de datos de la matriz utilizando el atributo 'dtype'
+print(arr.dtype)
+
+   ¿Qué pasa si un valor no se puede convertir?
+Si se proporciona un tipo en el que los elementos no se pueden convertir, NumPy generará un ValueError.
+
+ValueError: en Python, ValueError se genera cuando el tipo de argumento pasado a una función es inesperado/incorrecto.
+
+-Ejemplo 5
+
+Una cadena que no es un número entero como 'a' no se puede convertir a un número entero (generará un error):
+
+import numpy as np
+
+arr = np.array(['a', '2', '3'], dtype='i')
+
+Conversión de tipos de datos en matrices existentes
+La mejor manera de cambiar el tipo de datos de una matriz existente es hacer una copia de la matriz con el astype()método.
+
+La astype()función crea una copia de la matriz y le permite especificar el tipo de datos como parámetro.
+
+El tipo de datos se puede especificar usando una cadena, como 'f'flotante, 'i'entero, etc. o puede usar el tipo de datos directamente como floatflotante y intentero.
+
+-Ejemplo 6
+
+Cambie el tipo de datos de flotante a entero usando 'i'como valor de parámetro:
+# Importar la biblioteca NumPy con el alias 'np'
+import numpy as np
+
+# Crear una matriz NumPy a partir de una lista de números de punto flotante
+arr = np.array([1.1, 2.1, 3.1])
+
+# Convertir la matriz a un tipo de datos de enteros ('i')
+newarr = arr.astype('i')
+
+# Imprimir la nueva matriz
+print(newarr)
+
+# Imprimir el tipo de datos de la nueva matriz utilizando el atributo 'dtype'
+print(newarr.dtype)
+
+-Ejemplo 7
+Cambie el tipo de datos de flotante a entero usando intcomo valor de parámetro:
+
+# Importar la biblioteca NumPy con el alias 'np'
+import numpy as np
+
+# Crear una matriz NumPy a partir de una lista de números de punto flotante
+arr = np.array([1.1, 2.1, 3.1])
+
+# Convertir la matriz a un tipo de datos de enteros (utilizando la función int)
+newarr = arr.astype(int)
+
+# Imprimir la nueva matriz
+print(newarr)
+
+# Imprimir el tipo de datos de la nueva matriz utilizando el atributo 'dtype'
+print(newarr.dtype)
+
+-Ejemplo 8
+Cambie el tipo de datos de entero a booleano:
+
+# Importar la biblioteca NumPy con el alias 'np'
+import numpy as np
+
+# Crear una matriz NumPy a partir de una lista de enteros
+arr = np.array([1, 0, 3])
+
+# Convertir la matriz a un tipo de datos booleano
+newarr = arr.astype(bool)
+
+# Imprimir la nueva matriz
+print(newarr)
+
+# Imprimir el tipo de datos de la nueva matriz utilizando el atributo 'dtype'
+print(newarr.dtype)
+
+---------------------------NumPy Array Copy vs View--------------------------------
+
+La diferencia entre copiar y ver
+La principal diferencia entre una copia y una vista de una matriz es que la copia es una nueva matriz y la vista es solo una vista de la matriz original.
+
+La copia posee los datos y cualquier cambio realizado en la copia no afectará la matriz original, y cualquier cambio realizado en la matriz original no afectará la copia.
+
+La vista no posee los datos y cualquier cambio realizado en la vista afectará a la matriz original, y cualquier cambio realizado en la matriz original afectará a la vista.
 
 
+-Ejemplo 1
+
+Haga una copia, cambie la matriz original y muestre ambas matrices:
+# Importar la biblioteca NumPy con el alias 'np'
+import numpy as np
+
+# Crear una matriz NumPy a partir de una lista de enteros
+arr = np.array([1, 2, 3, 4, 5])
+
+# Realizar una copia de la matriz original
+x = arr.copy()
+
+# Modificar un elemento en la matriz original
+arr[0] = 42
+
+# Imprimir la matriz original después de la modificación
+print(arr)
+
+# Imprimir la copia de la matriz original
+print(x)
+
+nota: La copia NO DEBE verse afectada por los cambios realizados en la matriz original.
+
+-Ejemplo 2
+
+Cree una vista, cambie la matriz original y muestre ambas matrices:
+
+# Importar la biblioteca NumPy con el alias 'np'
+import numpy as np
+
+# Crear una matriz NumPy a partir de una lista de enteros
+arr = np.array([1, 2, 3, 4, 5])
+
+# Crear una vista de la matriz original
+x = arr.view()
+
+# Modificar un elemento en la matriz original
+arr[0] = 42
+
+# Imprimir la matriz original después de la modificación
+print(arr)
+
+# Imprimir la vista de la matriz original
+print(x)
+
+Nota: La vista DEBE verse afectada por los cambios realizados en la matriz original.
+
+-Ejemplo 3
+
+Cree una vista, cambie la vista y muestre ambas matrices:
+# Importar la biblioteca NumPy con el alias 'np'
+import numpy as np
+
+# Crear una matriz NumPy a partir de una lista de enteros
+arr = np.array([1, 2, 3, 4, 5])
+
+# Crear una vista de la matriz original
+x = arr.view()
+
+# Modificar un elemento en la vista
+x[0] = 31
+
+# Imprimir la matriz original después de la modificación en la vista
+print(arr)
+
+# Imprimir la vista de la matriz original
+print(x)
+
+Nota: La matriz original DEBE verse afectada por los cambios realizados en la vista.
+
+Compruebe si Array posee sus datos
+Como se mencionó anteriormente, las copias son propietarias de los datos y las vistas no son propietarias de los datos, pero ¿cómo podemos verificar esto?
+
+Cada matriz NumPy tiene el atributo baseque devuelve Nonesi la matriz posee los datos.
+
+De lo contrario, el base  atributo se refiere al objeto original.
+
+-Ejemplo 4
+
+Imprima el valor del atributo base para verificar si una matriz posee sus datos o no:
+# Importar la biblioteca NumPy con el alias 'np'
+import numpy as np
+
+# Crear una matriz NumPy a partir de una lista de enteros
+arr = np.array([1, 2, 3, 4, 5])
+
+# Crear una copia de la matriz original
+x = arr.copy()
+
+# Crear una vista de la matriz original
+y = arr.view()
+
+# Imprimir el atributo 'base' de la copia
+print(x.base)
+
+# Imprimir el atributo 'base' de la vista
+print(y.base)
 
 
-
-
-
-
-
-
+------------------------------NumPy Array Shape---------------------------------------
 
 
 
